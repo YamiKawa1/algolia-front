04dee156e83d0688f780477b994c45d45e16c03f
YamiKawa1 <jehanzao@gmail.com>
2024-06-23 23:35:21 -0300

    working good

--- app/cartSlice.ts
+++ app/cartSlice.ts
@@ -1,34 +1,62 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { v4 as uuid } from 'uuid'
 
 const initialState = {
-  items: {}, 
-  active: false,
+  id: '',
+  items: [{
+    id: 1,
+    name: 'hola',
+    price: 90.45,
+    quantity: 90,
+    imageSrc: 'sds'
+  }], 
 };
+
+const findIndex = (product_id:number, list:Array<any>) => {
+  const index = list.findIndex(product => product.id == product_id)
+  return index
+}
 
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
     addItem: (state:any, action) => {
-      const { id, img, name, quantity, price } = action.payload;
-      state.items[id] = { img, name, quantity, price };
+      const { id, imgURL, name, quantity, price } = action.payload;
+
+      if (state.items.length === 0 && id === '') {
+        state.id = uuid();
+      }
+      const index = findIndex(id, state.items)
+      
+      if (index != -1) {
+        state.items[index] = { id, imgURL, name, quantity, price };
+      } else{
+        state.items.push({ id,imgURL, name, quantity, price })
+      }
+      
+      localStorage.setItem("cart_id", state.id);
+      localStorage.setItem("cart_items", state.items);
     },
     removeItem: (state:any, action) => {
       const { id } = action.payload;
-      delete state.items[id];
+      const index = findIndex(id, state.items)
+      if (index != -1) {
+        delete state.items[index];
+        localStorage.setItem("cart_items", state.items);
+      }
     },
     updateQuantity: (state:any, action) => {
       const { id, quantity } = action.payload;
-      if (state.items[id]) {
-        state.items[id].quantity = quantity;
+      const index = findIndex(id, state.items)
+      if (index != -1) {
+        state.items[index].quantity = quantity
+        localStorage.setItem("cart_items", state.items);
       }
-    },
-    setActive: (state, action) => {
-      state.active = action.payload;
     },
   },
 });
 
-export const { addItem, removeItem, updateQuantity, setActive } = cartSlice.actions;
+export const { addItem, removeItem, updateQuantity } = cartSlice.actions;
 
 export default cartSlice.reducer;
