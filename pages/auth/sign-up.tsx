6ecd588328e4713ec6c3c92a615555409a178e01
YamiKawa1 <jehanzao@gmail.com>
2024-06-25 01:43:21 -0300

    sign up ready

--- pages/auth/sign-up.tsx
+++ pages/auth/sign-up.tsx
@@ -1,4 +1,64 @@
+import { useRouter } from "next/router"
+import { useState, useEffect } from "react"
+
+const BACKEND_URL = 'http://localhost:3333'
+
 export default function Example() {
+  const [email, setEmail] = useState('')
+  const [name, setName] = useState('')
+  const [phone, setPhone] = useState('')
+  const [password, setPassword] = useState('')
+  const [passEqual, setPassEqual] = useState(false)
+  const router = useRouter()
+
+  const verifyPasswords = (password2value:string) => {
+    if (password2value == password) {
+      setPassEqual(false)
+    } else {
+      setPassEqual(true)
+    }
+  }
+
+  const  handleSignUp = async () => {
+    const data = {
+      email,
+      password,
+      phone,
+      name
+    }
+
+    try {
+      var response = await fetch(`${BACKEND_URL}/auth/sign-up`, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },  
+        body: JSON.stringify(data)
+      })
+
+      if (response.status == 400) {
+        response = await response.json()
+        alert(response.message)
+      }
+
+      if (response.ok) {
+        response = await response.json()
+        console.log(response);
+        localStorage.setItem('token', response.body)
+        router.push('/')
+      }
+    } catch (error) {
+      alert(error)
+    }
+  }
+
+  useEffect(()=> {    
+    if (localStorage.getItem('token')){
+      router.push('/')
+    }
+  }, [])
+
     return (
       <>
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 mx-10 px-10">
@@ -14,35 +74,46 @@ export default function Example() {
           </div>
   
           <div className="mt-10 tablet:mx-auto tablet:w-full tablet:max-w-sm">
-            <form className="space-y-6" action="#" method="POST">
+            <div className="space-y-6">
               <div>
-                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                <label className="block text-sm font-medium leading-6 text-gray-900">
                   Email
                 </label>
                 <div className="mt-2">
                   <input
-                    id="email"
-                    name="email"
                     type="email"
                     autoComplete="email"
                     required
+                    onChange={(e)=> {setEmail(e.target.value)}}
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 tablet:w-96 tablet:text-sm tablet:leading-6 pl-2"
                   />
                 </div>
               </div>
 
               <div>
-                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                <label className="block text-sm font-medium leading-6 text-gray-900">
                   Nombre
                 </label>
                 <div className="mt-2">
                   <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
+                    type="text"
                     required
+                    onChange={(e)=> {setName(e.target.value)}}
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 tablet:text-sm tablet:leading-6 pl-2"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <label className="block text-sm font-medium leading-6 text-gray-900">
+                  Telefono
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="tel"
+                    onChange={(e)=> {setPhone(e.target.value)}}
+                    required
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 tablet:w-96 tablet:text-sm tablet:leading-6 pl-2"
                   />
                 </div>
               </div>
@@ -55,10 +126,8 @@ export default function Example() {
                 </div>
                 <div className="mt-2 ">
                   <input
-                    id="password"
-                    name="password"
                     type="password"
-                    autoComplete="current-password"
+                    onChange={(e)=> {setPassword(e.target.value)}}
                     required
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 tablet:text-sm tablet:leading-6 pl-2"
                   />
@@ -67,31 +136,31 @@ export default function Example() {
   
               <div>
                 <div className="flex items-center justify-between">
-                  <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                  <label className="block text-sm font-medium leading-6 text-gray-900">
                     Confirmar contraseña
                   </label>
                 </div>
                 <div className="mt-2">
                   <input
-                    id="password"
-                    name="password"
                     type="password"
-                    autoComplete="current-password"
+                    onChange={(e) => {verifyPasswords(e.target.value)}}
                     required
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 tablet:text-sm tablet:leading-6 pl-2"
                   />
                 </div>
+                <p className={passEqual? "text-red-500": "hidden"}>holis, tu contraseña no coinside</p>
               </div>
   
               <div>
                 <button
                   type="submit"
+                  onClick={() => {handleSignUp()}}
                   className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   Crear
                 </button>
               </div>
-            </form>
+            </div>
   
           </div>
         </div>
