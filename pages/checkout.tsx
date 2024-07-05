c70df7edad6f1b0229c04c75fd90b95acff6631f
YamiKawa1 <jehanzao@gmail.com>
2024-07-05 04:15:30 -0300

    checkout done

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,94 +1,206 @@
 import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid'
 import { useAtom } from 'jotai'
 import { CartProducts } from '@/app/atomsInitial'
+import { Fragment, useEffect, useState } from 'react'
+const BACKEND_URL = 'http://localhost:3333'
 
 export default function Example() {
   const [cart, setCart] = useAtom(CartProducts)
+  const [token, setToken] = useState('')
+  const [paymentPlatforms, setPaymentPlatforms] = useState([])
+  const [paymentPlatform, setPaymentPlatform] = useState(null)
+  const [address, setAddress] = useState('')
+  const [transferenceId, setTransferenceId] = useState('')
+  const [total, setTotal] = useState(0)
+
+  const [requireAddress, setRequireAddress] = useState('')
+
+  const totalBill = () => {
+    var total_bill = 0;
+    for (const product of cart){
+      total_bill += product.quantity * product.price
+    }
+    setTotal(total_bill)
+  }
+
+  const handleRequireAddress = () => {
+    if (requireAddress == 'no') {
+      return
+    } else {
+      return (
+          <Fragment>
+              <label className="mt-4 mb-2 block text-sm font-medium">Dirección completa</label>
+              <div className="relative">
+                <input type="text" defaultValue={address} onChange={(e) => {setAddress(e.target.value)}} className="w-full rounded-md border border-gray-200 px-4 py-3 pl-4 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="" />
+              </div>
+          </Fragment>
+      )
+    }
+  }
+
+  const handleRequireTransferenceId = () => {
+    if (paymentPlatform == 3) {
+      return
+    } else {
+      return (
+          <Fragment>
+              <label className="mt-4 mb-2 block text-sm font-medium">ID de transferencia</label>
+              <div className="relative">
+                <input type="text" onChange={(e) => {setTransferenceId(e.target.value)}} className="w-full rounded-md border border-gray-200 px-4 py-3 pl-4 text-sm uppercase shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" />
+                <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
+                </div>
+              </div>
+          </Fragment>
+      )
+    }
+  }
+
+  const showAccount = () => {
+    const platform = paymentPlatforms.find((platform) => platform.id == paymentPlatform)
+    if (platform) {
+      return platform.account  
+    } 
+  }
+
+  const handleMakePayment = async () => {
+    try {      
+      const send_data = {
+        payment_platform_id: paymentPlatform,
+        products: cart,
+        transference_id: transferenceId,
+        total: total + 4,
+        address: address
+      }
+      var url =  `${BACKEND_URL}/payments/make-payment`            
+      var response = await fetch(url, {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+          },
+          body: JSON.stringify(send_data)
+
+        })              
+        if (!response.ok){
+          response = await response.json()
+          alert(response.message)
+        }  else{
+          response = await response.json()
+          alert(response.message)
+          setPaymentPlatform(null)
+          setTotal(0)
+          setTransferenceId('')
+          setAddress('')
+        }
+    } catch (error) {
+      alert(error)
+    }
+  }
+  useEffect(()=> {
+    const token_storage = localStorage.getItem('token')
+    if (token) {      
+      setToken(token_storage)
+    }
+    totalBill()
+    const getPaymentsPlatforms = async() => {
+      try {
+        var url =  `${BACKEND_URL}/payments/see-payment-platforms`            
+        var response = await fetch(url, {
+            method: 'GET',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+            },
+          })              
+          if (!response.ok){
+            response = await response.json()
+            alert(response.message)
+          }  else{
+            response = await response.json()
+            setPaymentPlatforms(response.body)
+          }
+      } catch (error) {
+        alert(error)
+      }
+    }
+    const getUserDirection = async() => {
+      try {
+        var url =  `${BACKEND_URL}/user`            
+        var response = await fetch(url, {
+            method: 'GET',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+              'Authorization': 'Bearer ' + token
+            },
+          })              
+          if (!response.ok){
+            response = await response.json()
+            alert(response.message)
+          }  else{
+            response = await response.json()
+            
+            setAddress(response.body.address)
+          }
+      } catch (error) {
+        alert(error)
+      }
+    }
+
+    getPaymentsPlatforms()
+    if (token) {
+      getUserDirection
+    }
+  }, [cart, requireAddress])
 
   return (
-    <form>
       <div className="space-y-12 px-10 mx-10 pt-10">
         <div className="border-b border-gray-900/10 pb-12">
           <div className="mt-10 bg-gray-50 px-4 pt-8 lg:mt-0">
             <p className="text-xl font-medium">Detalles de pago</p>
-            <p className="text-gray-400">Complete your order by providing your payment details.</p>
             <div className="">
-
-            <select id="countries" className="bg-gray-50 rounded-md border border-gray-200 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mt-2">
-              <option selected>Elija un tipo de pago</option>
-              <option value="US">En casa</option>
-              <option value="CA">Pago movil Banco de Venezuela</option>
-              <option value="FR">Transferencia Banco de Venezuela</option>
-            </select>
-
-              <label className="mt-4 mb-2 block text-sm font-medium">Email</label>
-              <div className="relative">
-                <input type="text" id="email" name="email" className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="your.email@gmail.com" />
-                <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
-                  </svg>
-                </div>
-              </div>
-
-              <label className="mt-4 mb-2 block text-sm font-medium">Card Holder</label>
-              <div className="relative">
-                <input type="text" id="card-holder" name="card-holder" className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm uppercase shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="Your full name here" />
-                <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5zm6-10.125a1.875 1.875 0 11-3.75 0 1.875 1.875 0 013.75 0zm1.294 6.336a6.721 6.721 0 01-3.17.789 6.721 6.721 0 01-3.168-.789 3.376 3.376 0 016.338 0z" />
-                  </svg>
-                </div>
-              </div>
-
-              <label className="mt-4 mb-2 block text-sm font-medium">Card Details</label>
-              <div className="flex">
-                <div className="relative w-7/12 flex-shrink-0">
-                  <input type="text" id="card-no" name="card-no" className="w-full rounded-md border border-gray-200 px-2 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="xxxx-xxxx-xxxx-xxxx" />
-                  <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
-                    <svg className="h-4 w-4 text-gray-400" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                      <path d="M11 5.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1z" />
-                      <path d="M2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H2zm13 2v5H1V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1zm-1 9H2a1 1 0 0 1-1-1v-1h14v1a1 1 0 0 1-1 1z" />
-                    </svg>
-                  </div>
-                </div>
-                <input type="text" name="credit-expiry" className="w-full rounded-md border border-gray-200 px-2 py-3 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="MM/YY" />
-                <input type="text" name="credit-cvc" className="w-1/6 flex-shrink-0 rounded-md border border-gray-200 px-2 py-3 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="CVC" />
-              </div>
-
-              <label className="mt-4 mb-2 block text-sm font-medium">Billing Address</label>
-              <div className="flex flex-col sm:flex-row">
-                <div className="relative flex-shrink-0 sm:w-7/12">
-                  <input type="text" id="billing-address" name="billing-address" className="w-full rounded-md border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="Street Address" />
-                  <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
-                    <img className="h-4 w-4 object-contain" src="https://flagpack.xyz/_nuxt/4c829b6c0131de7162790d2f897a90fd.svg" alt="" />
-                  </div>
-                </div>
-                <select name="billing-state" className="w-full rounded-md border border-gray-200 px-4 py-3 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500">
-                  <option value="State">State</option>
+              <div className='relative'>
+                <select onClick={(e) => {setPaymentPlatform(e.target.value)}} className="bg-gray-50 rounded-md border border-gray-200 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mt-2">
+                  <option value=''>Elija un tipo de pago</option>
+                  {paymentPlatforms && paymentPlatforms.map((platform) => {
+                    return(
+                      <option value={platform.id}>{platform.name} {platform.account}</option>
+                    )
+                  })}
                 </select>
-                <input type="text" name="billing-zip" className="flex-shrink-0 rounded-md border border-gray-200 px-4 py-3 text-sm shadow-sm outline-none sm:w-1/6 focus:z-10 focus:border-blue-500 focus:ring-blue-500" placeholder="ZIP" />
               </div>
+              <p className='pl-4 m-2'>{showAccount()}</p>
+              <div className='relative'>
+                <select onClick={(e) => {setRequireAddress(e.target.value)}} className="bg-gray-50 rounded-md border border-gray-200 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 mt-2">
+                  <option value=''>Elija delivery o recoger en tienda</option>
+                  <option value='si'>Delivery</option>
+                  <option value='no'>Recoger en tienda</option>
+                </select>
+              </div>
+              
+              {handleRequireAddress()}
+              
+              {handleRequireTransferenceId()}
 
               <div className="mt-6 border-t border-b py-2">
                 <div className="flex items-center justify-between">
                   <p className="text-sm font-medium text-gray-900">Subtotal</p>
-                  <p className="font-semibold text-gray-900">$399.00</p>
+                  <p className="font-semibold text-gray-900">${total}</p>
                 </div>
                 <div className="flex items-center justify-between">
                   <p className="text-sm font-medium text-gray-900">Shipping</p>
-                  <p className="font-semibold text-gray-900">$8.00</p>
+                  <p className="font-semibold text-gray-900">$4.00</p>
                 </div>
               </div>
               <div className="mt-6 flex items-center justify-between">
                 <p className="text-sm font-medium text-gray-900">Total</p>
-                <p className="text-2xl font-semibold text-gray-900">$408.00</p>
+                <p className="text-2xl font-semibold text-gray-900">${total + 4}</p>
               </div>
             </div>
-            <button type="submit" className="mt-4 mb-8 w-full rounded-md bg-gray-900 px-6 py-3 font-medium text-white">Place Order</button>
+            <button type="submit" onClick={() => {handleMakePayment()}} className="mt-4 mb-8 w-full rounded-md bg-orange-500 hover:bg-orange-400 px-6 py-3 font-medium text-white">Place Order</button>
           </div>
         </div>
       </div>
-    </form>
   )
 }
