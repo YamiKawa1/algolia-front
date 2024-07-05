115cd4e2d6d3726a12ca39e547dea4aa9e47c90c
YamiKawa1 <jehanzao@gmail.com>
2024-07-05 02:34:12 -0300

    footer done

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -12,74 +12,31 @@ export const Footer = memo(function Footer() {
             <div className="label-semibold">Contactos</div>
               <ul className="small-regular flex flex-col gap-2">
                 <li>
-                  <Link
+                <p className='font-bold text-sm'>
+                  Telefono: 04128549665
+                  </p>
+
+                  {/* <Link
                     href={`/footer-link-${1 + 1}-${2 + 1}`}
                     title={`Numero de telefono:`}
                     className="text-neutral-darkest can-hover:transition-colors can-hover:hover:text-neutral-dark"
                     onClick={(e) => e.preventDefault()}
                   >
                                         Numero de telefono:
-                  </Link>
+                  </Link> */}
                 </li>
                 <li>
-                  <Link
+                  <p className='font-bold text-sm'>
+                  Correo: farmaciabarreto@gmail.com
+                  </p>
+                  {/* <Link
                     href={`/footer-link-${1 + 1}-${2 + 1}`}
                     title={`Link ${1 + 1}`}
                     className="text-neutral-darkest can-hover:transition-colors can-hover:hover:text-neutral-dark"
                     onClick={(e) => e.preventDefault()}
                   >
                     Correo:
-                  </Link>
-                </li>
-              </ul>
-          </div>
-          <div key={1} className="flex flex-col gap-2">
-            <div className="label-semibold">Contactos</div>
-              <ul className="small-regular flex flex-col gap-2">
-                <li>
-                  <Link
-                    href={`/footer-link-${1 + 1}-${2 + 1}`}
-                    title={`Numero de telefono:`}
-                    className="text-neutral-darkest can-hover:transition-colors can-hover:hover:text-neutral-dark"
-                    onClick={(e) => e.preventDefault()}
-                  >
-                                        Numero de telefono:
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href={`/footer-link-${1 + 1}-${2 + 1}`}
-                    title={`Link ${1 + 1}`}
-                    className="text-neutral-darkest can-hover:transition-colors can-hover:hover:text-neutral-dark"
-                    onClick={(e) => e.preventDefault()}
-                  >
-                    Correo:
-                  </Link>
-                </li>
-              </ul>
-          </div>
-          <div key={1} className="flex flex-col gap-2">
-            <div className="label-semibold">Contactos</div>
-              <ul className="small-regular flex flex-col gap-2">
-                <li>
-                  <Link
-                    href={`/footer-link-${1 + 1}-${2 + 1}`}
-                    title={`Numero de telefono:`}
-                    className="text-neutral-darkest can-hover:transition-colors can-hover:hover:text-neutral-dark"
-                    onClick={(e) => e.preventDefault()}
-                  >
-                                        Numero de telefono:
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href={`/footer-link-${1 + 1}-${2 + 1}`}
-                    title={`Link ${1 + 1}`}
-                    className="text-neutral-darkest can-hover:transition-colors can-hover:hover:text-neutral-dark"
-                    onClick={(e) => e.preventDefault()}
-                  >
-                    Correo:
-                  </Link>
+                  </Link> */}
                 </li>
               </ul>
           </div>
